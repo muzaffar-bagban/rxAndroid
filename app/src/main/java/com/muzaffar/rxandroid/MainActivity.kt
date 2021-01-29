d6706cc19cfb0240e1267ab345d2cbc0d1b43704
Muzaffar Bagban <muzaffarbagban@yahoo.in>
2021-01-29 20:25:58 +0530

    Create Operator

--- app/src/main/java/com/muzaffar/rxandroid/MainActivity.kt
+++ app/src/main/java/com/muzaffar/rxandroid/MainActivity.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.ObservableEmitter
+import io.reactivex.rxjava3.core.ObservableOnSubscribe
 import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.disposables.Disposable
@@ -22,8 +24,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val tasks = DataSource.createTasksList()
+        val task = tasks[0]
+
         val taskObservable: Observable<Task> = Observable
-                .fromIterable(DataSource.createTasksList())
+                .create(object : ObservableOnSubscribe<Task> {
+                    override fun subscribe(emitter: ObservableEmitter<Task>?) {
+                        emitter?.let {
+                            if (!it.isDisposed) {
+                                it.onNext(task)
+                            }
+                        } ?: return
+
+                    }
+                })
                 .subscribeOn(Schedulers.io())
                 .map {
                     Log.d(TAG, "inMap ${Thread.currentThread().name}")
